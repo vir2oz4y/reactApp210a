f37d2b3c020bf301931a473ed3ade530997e6655
student <andreigaivoronskikh120@gmail.com>
2023-03-23 09:52:22 +0700

    Добавление окон

--- src/Pages/StudentPage/GayvoronskikhAndrei/SiteMenu/SiteMenu.tsx
+++ src/Pages/StudentPage/GayvoronskikhAndrei/SiteMenu/SiteMenu.tsx
@@ -18,29 +18,50 @@ const SiteMenu = () => {
                     <ListItemButton
                         onClick={() => navigate('test1')}
                     >
-                        <ListItemText primary="Категории товаров"/>
+                        <ListItemText primary="Category"/>
                     </ListItemButton>
                 </ListItem>
                 <ListItem disablePadding>
                     <ListItemButton
                         onClick={() => navigate('test2')}
                     >
-                        <ListItemText primary="Клиенты"/>
+                        <ListItemText primary="Client"/>
                     </ListItemButton>
                 </ListItem>
                 <ListItem disablePadding>
                     <ListItemButton
                         onClick={() => navigate('test2')}
                     >
-                        <ListItemText primary="test2" />
+                        <ListItemText primary="Manufacturer" />
                     </ListItemButton>
                 </ListItem>
                 <ListItem disablePadding>
                     <ListItemButton
                         onClick={() => navigate('test2')}
                     >
-                        <ListItemText primary="Клиенты" />
+                        <ListItemText primary="Order" />
                     </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton
+                        onClick={() => navigate('test2')}
+                    >
+                        <ListItemText primary="Product" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton
+                        onClick={() => navigate('test2')}
+                    >
+                        <ListItemText primary="Purchase" />
+                    </ListItemButton>
+                    <ListItem disablePadding>
+                        <ListItemButton
+                            onClick={() => navigate('test2')}
+                        >
+                            <ListItemText primary="User" />
+                        </ListItemButton>
+                    </ListItem>
                 </ListItem>
             </List>
         </div>
