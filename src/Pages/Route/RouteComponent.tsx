e0c27a0918e69ba293207bdee0b4e9b27b40b4fa
NP4Ya <systemdeus451@gmail.com>
2023-03-23 09:39:32 +0700

    initial

--- src/Pages/Route/RouteComponent.tsx
+++ src/Pages/Route/RouteComponent.tsx
@@ -31,9 +31,9 @@ import AnikeevaVeraPage from "../StudentPage/AnikeevaVera/AnikeevaVeraPage";
 import {default as AnikeevaTest1Page} from "../StudentPage/AnikeevaVera/Test1/Test1";
 import {default as AnikeevaTest2Page} from "../StudentPage/AnikeevaVera/Test2/Test2";
 
-/*import Payzunov from "../StudentPage/Student/Payzunov";
-import {default as PayzunovTest1Page} from "../StudentPage/Student/Test 1/Test1Page";
-import {default as PayzunovTest2Page} from "../StudentPage/Student/Test 1/Test1Page";*/
+import Payzunov from "../StudentPage/Payzunov/payzunov";
+import {default as PayzunovTest1Page} from "../StudentPage/Payzunov/Test1/Test1Page";
+import {default as PayzunovTest2Page} from "../StudentPage/Payzunov/Test2/Test2Page";
 
 import TatarnikovEgorPage from "../StudentPage/TatarnikovEgor/TatarnikovEgorPage";
 import {default as MemePage1} from "../StudentPage/TatarnikovEgor/test1/test1page";
@@ -75,10 +75,10 @@ const RouteComponent = () => {
                         <Route path={'test2'} element={<SDBTest2Page/>}/>*/}
                     </Route>                 
                     
-                    {/*<Route path={'payzunov'} element={<Payzunov/>}>
-                        <Route path={'Test 1'} element={<PayzunovTest1Page/>}/>
-                        <Route path={'Test 2'} element={<PayzunovTest2Page/>}/>
-                    </Route>*/}
+                    {<Route path={'payzunov'} element={<Payzunov/>}>
+                        <Route path={'Test1'} element={<PayzunovTest1Page/>}/>
+                        <Route path={'Test2'} element={<PayzunovTest2Page/>}/>
+                    </Route>}
 
                     <Route path={'Gayvoronskikh'} element={<GayvoronskikhAndrei/>} >
                         <Route path={'test1'} element={<GayvoronskikhTest1Page/>} />
