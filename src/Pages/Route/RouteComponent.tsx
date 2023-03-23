695a29c3c0514838f7a568558707f798f423f310
Maks Skamer <bushmanov.03mak@mail.ru>
2023-03-23 09:42:23 +0700

    создал странички но надо не на руском

--- src/Pages/Route/RouteComponent.tsx
+++ src/Pages/Route/RouteComponent.tsx
@@ -24,8 +24,13 @@ import {default as Ses1} from "../StudentPage/SesNV/Test1/test1page";
 import {default as Ses2} from "../StudentPage/SesNV/Test2/test2page";
 
 import BushmanovMakPage from "../StudentPage/BushmanovMak/BushmanovMakPage";
-import {default as BushmanovTest1Page} from "../StudentPage/BushmanovMak/SiteMenu/Test1/Test1";
-import {default as BushmanovTest2Page} from "../StudentPage/BushmanovMak/SiteMenu/Test2/Test2";
+import {default as BushmanovTest1Page} from "../StudentPage/BushmanovMak/Категории товаров/Test1";
+import { default as BushmanovTest2Page } from "../StudentPage/BushmanovMak/Клиенты/Test2";
+import { default as BushmanovManufacturePage } from "../StudentPage/BushmanovMak/Мануфактуры/Manufacture";
+import { default as BushmanovOrderPage } from "../StudentPage/BushmanovMak/Заказы/Order";
+import { default as BushmanovProductPage } from "../StudentPage/BushmanovMak/Товары/Product";
+import { default as BushmanovPurchasePage } from "../StudentPage/BushmanovMak/Покупки/Purchase";
+import { default as BushmanovUserPage } from "../StudentPage/BushmanovMak/Пользователь/User";
 
 import AnikeevaVeraPage from "../StudentPage/AnikeevaVera/AnikeevaVeraPage";
 import {default as AnikeevaTest1Page} from "../StudentPage/AnikeevaVera/Test1/Test1";
@@ -97,7 +102,12 @@ const RouteComponent = () => {
 
                     <Route path={'bushmanov'} element={<BushmanovMakPage/>} >
                         <Route path={'Test1'} element={<BushmanovTest1Page/>} />
-                        <Route path={'Test2'} element={<BushmanovTest2Page/>} />
+                        <Route path={'Test2'} element={<BushmanovTest2Page />} />
+                        <Route path={'Manufacture'} element={<BushmanovManufacturePage />} />
+                        <Route path={'Order'} element={<BushmanovOrderPage />} />
+                        <Route path={'Product'} element={<BushmanovProductPage />} />
+                        <Route path={'Purchase'} element={<BushmanovPurchasePage />} />
+                        <Route path={'User'} element={<BushmanovUserPage />} />
                     </Route>
 
                     <Route path={'BurlakAD'} element={<BurlakAD/>}>
--- src/Pages/StudentPage/BushmanovMak/SiteMenu/SiteMenu.tsx
+++ src/Pages/StudentPage/BushmanovMak/SiteMenu/SiteMenu.tsx
@@ -13,12 +13,37 @@ const SiteMenu = () => {
             <List>
                 <ListItem disablePadding>
                     <ListItemButton onClick={()=>navigate('Test1')}>
-                        <ListItemText primary="Test1" />
+                        <ListItemText primary="Category" />
                     </ListItemButton>
                 </ListItem>
                 <ListItem disablePadding>
                     <ListItemButton onClick={()=>navigate('Test2')}>
-                        <ListItemText primary="Test2" />
+                        <ListItemText primary="Client" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton onClick={() => navigate('Test2')}>
+                        <ListItemText primary="Manufacture" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton onClick={() => navigate('Test2')}>
+                        <ListItemText primary="Order" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton onClick={() => navigate('Test2')}>
+                        <ListItemText primary="Product" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton onClick={() => navigate('Test2')}>
+                        <ListItemText primary="Purchase" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton onClick={() => navigate('Test2')}>
+                        <ListItemText primary="User" />
                     </ListItemButton>
                 </ListItem>
             </List>
