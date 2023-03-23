fdd5328e3119be08a0749d84a1c8892fa66c9ee6
SesNV <sesnadia03@gmail.com>
2023-03-23 09:52:04 +0700

    страница

--- src/Pages/StudentPage/SesNV/SesNV.tsx
+++ src/Pages/StudentPage/SesNV/SesNV.tsx
@@ -4,14 +4,22 @@ import ContentBlock from "../../../Components/ContentBlock/ContentBlock";
 import Menu2 from "./Menu2/Menu2";
 import {Outlet} from "react-router-dom";
 
+
 const SesNV = () => {
     return (
         <div>
             <Header studentFio={'Сесь Надежда'}/>
 
             <ContentBlock>
-                <Menu2/>
-                <Outlet/>
+                <div style={{ display: 'flex', gap: '1em' }}>
+                    <div style={{ width: '200px' }}>
+                        <Menu2 />
+                    </div>
+
+                    <div style={{ padding:'1em' }}>
+                        <Outlet />
+                    </div>
+                </div>
             </ContentBlock>
         </div>
     );
