75e5af46bb3a7bd7a753efd5200f0de31a4983d3
Veraanik <124228082+Veraanik@users.noreply.github.com>
2023-04-06 09:54:50 +0700

    Category

--- src/Pages/StudentPage/AnikeevaVera/Category/Category.tsx
+++ src/Pages/StudentPage/AnikeevaVera/Category/Category.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import {Box} from "@mui/material";
+import Button from "@mui/material/Button";
 
 const Category = () => {
+    const columns: GridColDef[] = [
+        {
+            field: 'id',
+            headerName: 'ID',
+            width: 100,
+            editable: true,
+        },
+        {
+            field: 'name',
+            headerName: 'Category',
+            width: 150,
+            editable: true,
+        },
+        {
+            field: '',
+            headerName: '',
+            width: 200,
+            renderCell:(e:any) =>{
+                return <div style={{display: 'flex', gap:'lem'}}>
+                    <Button color ={'primary'} variant = {'contained'}>
+                        EDIT
+                    </Button>
+                    <Button color ={'primary'} variant = {'contained'}>
+                        DELETE
+                    </Button>
+                </div>
+            }
+        },
+
+    ];
+    const rows = [
+        { id: 1, name: 'category 1' },
+        { id: 2, name: 'category 2' },
+        { id: 3, name: 'category 3' },
+        { id: 4, name: 'category 4' },
+        { id: 5, name: 'category 5' },
+        { id: 6, name: 'category 6' },
+
+    ];
+
     return (
-        <div>
-            Category
-        </div>
+        <Box sx={{ height: '70vh', width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+            />
+        </Box>
     );
 };
 
