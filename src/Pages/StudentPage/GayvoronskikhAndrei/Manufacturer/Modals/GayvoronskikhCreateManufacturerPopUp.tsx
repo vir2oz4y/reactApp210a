3f8e03c1de26fbd9bf5bb9d3001de97aa4b99de0
amitafw <amit.afw1@gmail.com>
2023-04-27 11:48:28 +0700

    Добавлены мануфактуры

---
+++ src/Pages/StudentPage/GayvoronskikhAndrei/Manufacturer/Modals/GayvoronskikhCreateManufacturerPopUp.tsx
@@ -0,0 +1,52 @@
+import { Button, TextField } from '@mui/material'
+import React from 'react'
+import { useState } from 'react'
+import GayvoronskikhPopUp, { IPopUp } from '../../../../../Components/Gayvoronskikh/GayvoronskikhPopUp/GayvoronskikhPopUp'
+import { Category } from '../model'
+
+type Props = IPopUp & {
+
+    onCreate: (newCategory: Category) => void;
+}
+
+export const GayvoronskikhCreateCategoryPopUp = ({ open, onClose,onCreate }:Props) => {
+
+    const [CategoryName, setCategoryName]= useState('')
+    const onCreateClick = () => {
+        onCreate({
+            id: Math.random(),
+            name: CategoryName
+        })
+        onClose()
+    }
+
+    return (
+        <div>
+            <GayvoronskikhPopUp
+            open={open}
+            onClose={onClose}
+            title={'Create Category'}
+            >
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '1em' }}>
+
+                    <TextField
+                        id="standard-basic"
+                        label="Standard"
+                        variant="standard"
+                        value={CategoryName}
+                        onChange={(e)=>setCategoryName(e.target.value)}
+                    />
+                        <div style={{ display: 'flex', justifyContent:'center'}}>
+                        <Button
+                            color={'primary'}
+                            variant={'contained'}
+                            onClick={()=>onCreateClick()}
+                        >
+                                 Create
+                         </Button>
+                        </div>
+                </div>
+        </GayvoronskikhPopUp>
+    </div>
+    )
+}
